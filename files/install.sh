04d8eedfb6d4c1b9089e215f6898a03c51da639b
macwinnie <dev@macwinnie.me>
2021-05-02 20:09:27 +0000

    chown on image creation

--- files/install.sh
+++ files/install.sh
@@ -4,10 +4,12 @@ wget $(curl -L https://community.limesurvey.org/downloads/ | sed -n "s/.*'\(.*lt
 
 unzip /tmp/limesurvey.zip -d /tmp/
 
-mv  /tmp/limesurvey  /tmp/html
-mv  /tmp/html        "${APACHE_WORKDIR}"/..
+mv  /tmp/limesurvey  "${APACHE_WORKDIR}"
 rm  -rf  /tmp/*
 
 mv  /docker_install/templates               /templates
 mv  /docker_install/01_install_on_start.sh  /boot.d/
 rm  -rf  /docker_install
+
+chown -R www-data:www-data "${APACHE_WORKDIR}"
+
